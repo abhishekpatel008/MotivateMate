a24ca12d3fa89032ef7534428b9211c8024d98eb
Abhishek Patel <apate320@charlotte.edu>
2026-04-01 11:16:06 -0400

    Fixed some deployment Issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,31 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {  // <========= Function, not object
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) {
-              return 'react-vendor';
-            }
-            if (id.includes('react-router')) {
-              return 'router-vendor';
-            }
-            return 'vendor';
-          }
-        }
-      }
-    }
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
-      }
-    }
+    outDir: 'dist'
   }
 })
